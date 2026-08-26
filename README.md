fe96f7558c9335bbda400d073a39ee9af3766c13
bigred <cry48435@gmail.com>
2026-08-27 06:23:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# flask_cicd_pratice
---
+++ flask_app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app_100  = Flask(__name__)
+
+@app_100.route("/")
+def index():
+	return "This is index"
+
+@app_100.route("/hello")
+def hello():
+        return "<h1>Hello World!</h1>"
+
+@app_100.route("/healthz")
+def healthz():
+	return {"status":"ok"}
+
+if __name__ == "__main__":
+	app_100.run(host="0.0.0.0", port=5000)
